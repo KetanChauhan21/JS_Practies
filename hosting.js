726b7649f4668d44b5150d616ffe55ef96dce2c8
KetanChauhan21 <ketanchauhan4356@gmail.com>
2026-01-30 16:11:38 +0530

    30/1/2026

--- hosting.js
+++ hosting.js
@@ -4,12 +4,57 @@
 // let a = "name";
 // var b = 3
 
-var a = 4;
-function add(number) {
-  var addval = number + number;
-  return addval;
+// var a = 4;
+// function add(number) {
+//   var addval = number + number;
+//   function val(){
+//     console.log(addval)
+//   }
+// }
+
+// console.log(a);
+// var first = add(2);
+// var second = add(4);
+
+
+// console.log(b)
+// let a = 3
+// var b = 100
+
+
+// const arr = [11,2,3,1]
+
+// const arr1 = arr.map((val)=>{
+//   return val+1;
+// })
+
+// const arr2 = arr.filter((re)=>{
+//   return re>5;
+// })
+
+// const arr3 = arr.reduce((curr, prev)=>{
+//   return prev+curr;
+// })
+
+// console.log(typeof arr1)
+// console.log(typeof arr2)
+// console.log(typeof arr3)
+
+
+// console.log(arr1)
+// console.log(arr2)
+// console.log(arr3)
+
+
+var c = 3;
+let k = 11;
+
+if(true){
+  console.log("sum")
+  let a = 43
+  var d = 1000
+  a++ 
+  console.log(a)
 }
 
-console.log(a);
-var first = add(2);
-var second = add(4);
+console.log(c)
