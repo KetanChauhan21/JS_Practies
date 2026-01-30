655c86bb94c0fee6f79f9d4d66a6a07c94bdc769
KetanChauhan21 <ketanchauhan4356@gmail.com>
2026-01-30 12:24:19 +0530

    Done 30/1/2025

---
+++ hosting.js
@@ -0,0 +1,15 @@
+// console.log(b)
+// console.log(a)
+
+// let a = "name";
+// var b = 3
+
+var a = 4;
+function add(number) {
+  var addval = number + number;
+  return addval;
+}
+
+console.log(a);
+var first = add(2);
+var second = add(4);
